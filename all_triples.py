59cad4d46fed1aef72f8b71f410597bd7e430e9c
Michael Ellis <michael.f.ellis@gmail.com>
2019-01-01 17:23:44 -0500

    Change constraint scheme.
    Pitches are now constrained within lower and upper midi limits which may be specified from the command line.

--- all_triples.py
+++ all_triples.py
@@ -3,8 +3,15 @@
 """
 etudes for ear training based on the pentatonic scale plus the 4th degree of the diatonic scale.
 """
+import re
 from itertools import permutations
 from random import shuffle
+
+
+# LUT for diatonic interval to midi interval
+d2m = {1:0, 2:2, 3:4, 4:5, 5:7, 6:9, 7:11}
+
+
 
 class Triple:
     def __init__(self, pitchnums):
@@ -26,8 +33,10 @@ def shuffledTriples():
 
 def intervalAsc(p1,p2):
     """ Return the ascending interval between two pitches numbers. """
+    ## Convert from base 7 index 1 to base 7 index 0
     a = p1 - 1
     b = p2 - 1
+    ## subtract, reduce modulo 7, convert back to 1 indexed.
     return 1 + (b - a) % 7
 
 def intervalReduced(p1, p2):
@@ -35,6 +44,117 @@ def intervalReduced(p1, p2):
     assert 1 <= p1 <= 7 and 1 <= p2 <= 7
     i = intervalAsc(p1, p2)
     return i if i <= 4 else -(9 - i)
+
+def midiFirst(keynum, harmonic=False):
+    """
+    Return midi number of first pitch. Tbon assumes a prior pitch of middle C (60) or
+    middle C# (61) depending on the key signature.
+    """
+    cdegreemidi = { # LUT by keynum
+            0:(1,60), 1:(7,60), 2:(7,61), 3:(6,60),
+            4:(6,61), 5:(5,60), 6:(4,59), 7:(4,60),
+            8:(3,60), 9:(3,61), 10:(2,60), 11:(2,61)
+            }
+    d0,m0 = cdegreemidi[keynum]
+    if keynum == 5 and harmonic:
+        m0 += 1
+    return m0, d0
+
+
+def midiNext(m0, d0, d1, harmonic=False):
+    """
+    Return midi number of diatonic pitch number d1 given preceding midi and diatonic
+    pitch numbers m0 and d0.
+    """
+    if d0 == d1:
+        return m0 ## same pitch
+    ir = intervalReduced(d0, d1)
+    extra = 0
+    if harmonic:
+        if d0 == 5:
+            extra = -1
+        elif d1 == 5:
+            extra = 1
+    if ir >= 1 and d1 > d0:
+        ## positive ir, d1 higher
+        return m0 + d2m[d1] - d2m[d0] + extra
+    elif ir >= 1 and d1 < d0:
+        ## positive ir, d1 lower
+        return m0 + 12 + d2m[d1] - d2m[d0] + extra
+    elif ir < 1 and d1 > d0:
+        ## negative ir, d1 higher
+        return m0 - 12 + d2m[d1] - d2m[d0] + extra
+    elif ir < 1 and d1 < d0:
+        ## negative ir, d1 lower
+        return m0 + d2m[d1] - d2m[d0] + extra
+
+def computeOctaveOffset(mlo, m, mhi):
+    """
+    Return an integer number of octaves needed to move midi pitch m within
+    the inclusive interval between mlo and mhi.
+    """
+    assert 0 <= mlo < mhi < 128  ## require valid midi range
+    offset12 = 0
+    while not(mlo <= offset12 + m <= mhi):
+        if offset12 + m < mlo:
+            offset12 += 12
+        else:
+            offset12 -= 12
+
+    # done. convert to octaves and return
+    return offset12 // 12
+
+def computeTripleOffset(m0, d0, t, mlo, mhi, harmonic=False):
+    """
+    m0 : prior midi pitch
+    d0 : prior diatonic pitch
+    t : Triple instance
+    mlo: midi low limit
+    mhi: midi high limit
+    Sets the offset attribute of t, if possible
+    Returns (True, last midi) if an offset value is found that can successfully
+    place all pitches within the range from mlo to mhi inclusive.
+    Returns (False, None) if not (and leave t.offset unaltered).
+    """
+    print()
+    trialoffset=0
+    tries = 0
+    while True:
+        midinums = []
+        m = m0
+        d = d0
+        for n in t.nums:
+            m = midiNext(m, d, n, harmonic)
+            d = n
+            midinums.append(m)
+
+        offsets = []
+        for m in midinums:
+            offsets.append(computeOctaveOffset(mlo, m + (12*trialoffset), mhi))
+
+        success = all([offset == offsets[0] for offset in offsets])
+        if success:
+            t.offset = offsets[0] if tries == 0 else trialoffset
+            return success, midinums[2] + 12*t.offset, t.nums[2]
+
+        elif tries < 3:
+            print(tries, offsets)
+            trialoffset = offsets[tries]
+            tries += 1
+            continue
+        else:
+            return success, None, None ## failed
+
+def constrain2(triples, mlo, mhi, keynum, harmonic=False):
+    """
+    Compute offsets for each triple such that all pitches are within
+    the interval mlo:mhi inclusive.
+    """
+    m, d = midiFirst(keynum, harmonic)
+    for t in triples:
+        success, m, d = computeTripleOffset(m, d, t, mlo, mhi, harmonic)
+        assert success
+
 
 def excursionMinMax(pitches):
     """ Return a tuple of min and max excursions of a sequence of pitches. """
@@ -86,7 +206,7 @@ def constrain(triples, lo8=-2, hi8=2):
         so = sumOfOffsets(sublist)
         xlo = xmin + so * 8
         xhi = xmax + so * 8
-        print("{}: {}, {}, {}, {}, {}".format(i,xmin, xmax, so,xlo,xhi))
+        #print("{}: {}, {}, {}, {}, {}".format(i,xmin, xmax, so,xlo,xhi))
         if xlo < lo:
             triples[i].offset += 1 # raise last triple by one octave
         elif xhi > hi:
@@ -163,11 +283,41 @@ def bins(triples):
 
     return result
 
+def getKeyNum(directives):
+    """
+    Find the K= directive, extract the keyname, return the number from the LUT.
+    """
+    lut = {
+        'C':0,
+        'D@':1,
+        'D':2,
+        'E@':3,
+        'E':4,
+        'F':5,
+        'G@':6,
+        'G':7,
+        'A@':8,
+        'A':9,
+        'B@':10,
+        'B':11,
+        }
+    pat = r"[^K]*K=([A-G]@?).*"
+    m = re.match(pat, directives)
+    if m is None:
+        ## not found, assume key of C.
+        return 0
+    kstring = m.groups()[0]
+    if kstring not in lut:
+        return 0
+    return lut[kstring]
 
-def etude(triples, directives="K=E@ T=120", countin="z - - - |", hminor=False):
+
+
+def etude(mlo, mhi, triples, directives="K=E@ T=120", countin="z - - - |", hminor=False):
     """ Return lines of Tbon notation, one line for each possible triple."""
     e = [directives, countin]
-    constrain(triples, -1, 1)
+    kn = getKeyNum(directives)
+    constrain2(triples, mlo, mhi, kn, hminor)
     for t in triples:
         l = line(t)
         if hminor:
@@ -176,13 +326,13 @@ def etude(triples, directives="K=E@ T=120", countin="z - - - |", hminor=False):
     return "\n\n".join(e)
 
 
-def mkEtudes(directives="K=E@ T=120", countin="z - - - |"):
+def mkEtudes(mlo, mhi, directives="K=E@ T=120", countin="z - - - |"):
     """ Return 4 etudes, 1 for each bin """
     triples = shuffledTriples()
     tbins = bins(triples)
-    etudes = [etude(tbin, directives, countin) for tbin in tbins[0:4]]
-    etudes.append(etude(tbins[4], directives, countin, hminor=True))
-    etudes.append(etude(tbins[5], directives, countin, hminor=True))
+    etudes = [etude(mlo, mhi, tbin, directives, countin) for tbin in tbins[0:4]]
+    etudes.append(etude(mlo, mhi, tbins[4], directives, countin, hminor=True))
+    etudes.append(etude(mlo, mhi, tbins[5], directives, countin, hminor=True))
     return etudes
 
 
@@ -191,8 +341,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Etude Generator")
     parser.add_argument('-d', "--directives", type=str, default="K=E@ T=120",
                         help='Tbon directives. Default="K=E@ T=120"')
+    parser.add_argument('-u', '--midihigh', type=int, default=84,
+            help='High limit for pitches as a midi number. Default=84')
+    parser.add_argument('-l', '--midilow', type=int, default=36,
+            help='Low limit for pitches as a midi number. Default=36')
+
     args = parser.parse_args()
     outfiles = ("pentatonic.tbn", "plus4.tbn", "plus7.tbn", "both47.tbn", "harmonic5.tbn", "harmonic47.tbn")
-    for outname, etd in zip(outfiles, mkEtudes(directives=args.directives)):
+    for outname, etd in zip(outfiles, mkEtudes(args.midilow, args.midihigh,
+                                               directives=args.directives)):
         with open(outname, 'w') as f:
             print(etd, file=f)
